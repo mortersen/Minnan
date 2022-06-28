a7e7722622d71e5f27dcecc1a04da200f69fc40b
CYCY <mortersen@163.com>
2022-06-28 21:29:55 +0800

    add ignore

--- ExpertWidget.py
+++ ExpertWidget.py
@@ -4,6 +4,7 @@ from PyQt5.QtSql import QSqlQuery,QSqlQueryModel
 from CreateDBConnect import SingleDBConnect
 from MinnanCSIRDB import MainWindow
 
+eachRecordPerPage = 12
 
 #专家学者页面
 class ExpertIndexWidget(QWidget):
@@ -17,13 +18,45 @@ class ExpertIndexWidget(QWidget):
 
         #普通数据库链接对象
         self.sqlQuery = QSqlQuery(SingleDBConnect().DB)
+        self.totoalRecord = self.countRecord("")
+        self.currentPage = 0
+        self.condition = ""
+        self.totoalPage = self.countPage()
+
         #与表格关联数据库链接对象
         self.qryModel = QSqlQueryModel(self)
+        self.ui.tableWidget.setModel(self.qryModel)
+        self.query = "SELECT Name,Post,,ResearchFindings,,ID from Institution  "
 
 
     #初始化表格
     def initTable(self):
         pass
+
+    #按条件统计数据库记录数
+    def countRecord(self,condition):
+        SqlTotoalQuery = "SELECT 1 from Expert" + condition
+        try:
+            self.sqlQuery.exec(SqlTotoalQuery)
+            self.sqlQuery.last()
+            return self.sqlQuery.at() + 1
+        except Exception:
+            print(Exception.__str__())
+
+    #计算页数
+        # 统计总页数
+    def countPage(self):
+            if self.totoalRecord > 0:
+                if self.totoalRecord > eachRecordPerPage:
+                    countPages = self.totoalRecord // eachRecordPerPage
+                    if self.totoalRecord % eachRecordPerPage != 0:
+                        return countPages + 1
+                    else:
+                        return countPages
+                else:
+                    return 1
+            else:
+                return 1
 
 if __name__ == '__main__':
     pass
